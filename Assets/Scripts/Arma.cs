74d3cc6e37c0d01088f115622ba5ebbf887b27bb
Thiago Adriano <thiago.adriano26@gmail.com>
2019-04-07 23:41:11 -0300

    first commit

---
+++ Assets/Scripts/Arma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour {
+
+    public float damage;
+    public float range;
+    public float firerate;
+    public float waitToFirerate;
+    public Camera cam;
+
+    public ParticleSystem particulaArma;
+    public ParticleSystem impacto;
+    public bool hold;
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
