1be7b565237afc49591ba83c0b31145da0680525
Thiagtao da Silva Adriano <tadriano@band.com.br>
2019-04-08 18:29:25 -0300

    add gum

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -1,21 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arma : MonoBehaviour {
+public class Arma : MonoBehaviour
+{
 
+    private int TotalMunicao = 30;
+
+    //"Dano da arma"
     public float damage;
+
+    //Distancia do tiro
     public float range;
+
+    //Quantos tiros por segundos
     public float firerate;
     public float waitToFirerate;
+
+    //Camera
     public Camera cam;
 
+    //Particulas
     public ParticleSystem particulaArma;
-    public ParticleSystem impacto;
+    public GameObject impacto;
+
     public bool hold;
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            hold = true;
+
+        if (Input.GetButtonUp("Fire1"))
+            hold = false;
+
+        if (hold)
+            waitToFirerate += 1;
+
+        if (waitToFirerate > firerate && TotalMunicao > 0)
+            Atira();
+
+    }
+
+    void Atira()
+    {
+
+        waitToFirerate = 0;
+        particulaArma.Play();
+        RaycastHit hit;
+        TotalMunicao -= 1;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log("Fire" + hit.transform.name);
+            GameObject impactoAlvo = Instantiate(impacto, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+
+    }
 }
