8279d49d377937ac205c7ba95353697393129c93
Thiagtao da Silva Adriano <tadriano@band.com.br>
2019-04-09 14:19:32 -0300

    add zombie

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Arma : MonoBehaviour
 {
 
     private int TotalMunicao = 30;
     private int DestroyWall = 10;
+    private int damageZombie = 1;
 
     //"Dano da arma"
     public float damage;
@@ -52,7 +54,7 @@ public class Arma : MonoBehaviour
         particulaArma.Play();
         RaycastHit hit;
         TotalMunicao -= 1;
-        
+
 
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
@@ -63,12 +65,29 @@ public class Arma : MonoBehaviour
             {
                 Destroy(hit.collider.gameObject);
                 DestroyWall -= 1;
-            }           
-            
+            }
+
+
+            if (hit.transform.tag == "zombie")
+            {
+                damageZombie -= 1;
+
+                if (damageZombie == 0)
+                {
+                    var zAnim = hit.transform.GetComponent<Animator>();
+                    var zAi = hit.transform.GetComponent<NavMeshAgent>();
+
+                    zAi.Stop();
+                    zAnim.SetBool("zombieWalk", false);
+                    zAnim.SetBool("zombieDie", true);
+                }
+            }
+
         }
 
         if (DestroyWall == 0)
         {
+            Zombie.zombieCanWalk = true;
             Destroy(GameObject.FindWithTag("wall_block"));
         }
 
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Zombie : MonoBehaviour
+{
+    private NavMeshAgent _nav;
+    private Transform _player;
+    Animator _anim;
+    public static bool zombieCanWalk = false;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _anim.SetBool("zombieWalk", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zombieCanWalk)
+        {
+            _nav.destination = _player.position;
+        }
+    }
+
+}
